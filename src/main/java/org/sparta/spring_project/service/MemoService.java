b6d8ae7494dc7223a1c15356c94fbf7fc9d288d8
hsd9681 <hsd9681@naver.com>
2024-05-13 16:12:09 +0900

    add: Memorepository 생성 refactor:service->repository 분리

--- src/main/java/org/sparta/spring_project/service/MemoService.java
+++ src/main/java/org/sparta/spring_project/service/MemoService.java
@@ -3,6 +3,7 @@ package org.sparta.spring_project.service;
 import org.sparta.spring_project.dto.MemoRequestDto;
 import org.sparta.spring_project.dto.MemoResponseDto;
 import org.sparta.spring_project.entitiy.Memo;
+import org.sparta.spring_project.repository.Memorepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -27,23 +28,8 @@ public class MemoService {
         Memo memo = new Memo(requestDto);
 
         // DB 저장
-        KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환받기 위한 객체
-
-        String sql = "INSERT INTO memo (username, contents) VALUES (?, ?)";
-        jdbcTemplate.update( con -> {
-                    PreparedStatement preparedStatement = con.prepareStatement(sql,
-                            Statement.RETURN_GENERATED_KEYS);
-
-                    preparedStatement.setString(1, memo.getUsername());
-                    preparedStatement.setString(2, memo.getContents());
-                    return preparedStatement;
-                },
-                keyHolder);
-
-        // DB Insert 후 받아온 기본키 확인
-        Long id = keyHolder.getKey().longValue();
-        memo.setId(id);
-
+        Memorepository memoRepository = new Memorepository(jdbcTemplate);
+        Memo saveMemo =  memoRepository.save(memo);
         // Entity -> ResponseDto
         MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
 
@@ -52,29 +38,23 @@ public class MemoService {
 
     public List<MemoResponseDto> getMemos() {
 
-        // DB 조회
-        String sql = "SELECT * FROM memo";
+        //db 조회
+        Memorepository memoRepository = new Memorepository(jdbcTemplate);
+        return memoRepository.findAll();
 
-        return jdbcTemplate.query(sql, new RowMapper<MemoResponseDto>() {
-            @Override
-            public MemoResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-                // SQL 의 결과로 받아온 Memo 데이터들을 MemoResponseDto 타입으로 변환해줄 메서드
-                Long id = rs.getLong("id");
-                String username = rs.getString("username");
-                String contents = rs.getString("contents");
-                return new MemoResponseDto(id, username, contents);
-            }
-        });
+        // DB 조회
+
     }
 
     public Long updateMemo(Long id,MemoRequestDto requestDto) {
+        Memorepository memoRepository = new Memorepository(jdbcTemplate);
+
+
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = findById(id);
+        Memo memo = memoRepository.findById(id);
         if(memo != null) {
             // memo 내용 수정
-            String sql = "UPDATE memo SET username = ?, contents = ? WHERE id = ?";
-            jdbcTemplate.update(sql, requestDto.getUsername(), requestDto.getContents(), id);
-
+            memoRepository.update(id, requestDto);
             return id;
         } else {
             throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
@@ -83,13 +63,14 @@ public class MemoService {
 
 
     public Long deleteService(Long id) {
-        // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = findById(id);
-        if(memo != null) {
-            // memo 삭제
-            String sql = "DELETE FROM memo WHERE id = ?";
-            jdbcTemplate.update(sql, id);
+        Memorepository memoRepository = new Memorepository(jdbcTemplate);
 
+        // 해당 메모가 DB에 존재하는지 확인
+        Memo memo = memoRepository.findById(id);
+        if(memo != null) {
+
+            memoRepository.delete(id);
+            // memo 삭제
             return id;
         } else {
             throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
@@ -98,21 +79,7 @@ public class MemoService {
 
 
 
-    private Memo findById(Long id) {
-        // DB 조회
-        String sql = "SELECT * FROM memo WHERE id = ?";
 
-        return jdbcTemplate.query(sql, resultSet -> {
-            if(resultSet.next()) {
-                Memo memo = new Memo();
-                memo.setUsername(resultSet.getString("username"));
-                memo.setContents(resultSet.getString("contents"));
-                return memo;
-            } else {
-                return null;
-            }
-        }, id);
-    }
 
 
 }
