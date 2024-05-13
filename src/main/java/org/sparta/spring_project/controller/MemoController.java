a14e28244b30992fe28f4efb63741d96f126a008
hsd9681 <hsd9681@naver.com>
2024-05-13 14:56:23 +0900

    add: memo 수정, 삭제api

--- src/main/java/org/sparta/spring_project/controller/MemoController.java
+++ src/main/java/org/sparta/spring_project/controller/MemoController.java
@@ -43,4 +43,27 @@ public class MemoController {
         return responseList;
     }
 
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto){
+        if(memoList.containsKey(id)){
+            Memo memo = memoList.get(id);
+
+            memo.update(requestDto);
+            return memo.getId();
+        }else{
+            throw  new IllegalArgumentException("Memo not found");
+        }
+    }
+
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id){
+        if(memoList.containsKey(id)){
+            memoList.remove(id);
+            return id;
+        }else {
+            throw  new IllegalArgumentException("Memo not found");
+        }
+    }
+
 }
